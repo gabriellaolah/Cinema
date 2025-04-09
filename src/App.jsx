88c77f1e1d0a4da7b12e882e0a3f252c0b9b4f91
gaol <gaol@itu.dk>
2025-04-09 13:38:12 +0200

    Initial setup with React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white font-mono flex flex-col justify-center items-center p-10">
+      <h1 className="text-5xl font-bold glitch">🎞️ Husets Biograf</h1>
+      <p className="text-lg mt-4 text-red-500">If you see this, Tailwind is working ❤️</p>
+    </div>
+  );
+}
+
+export default App;
